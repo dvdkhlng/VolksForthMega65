bbd5bb3b753feee60920a483c6e2f3f954882248
David <dvdkhlng@posteo.de>
2026-02-28 13:05:20 +0100

    c65: handle "holes" in the forth block layout when generating .d81.

--- 6502/C64/disks/build-d81.sh
+++ 6502/C64/disks/build-d81.sh
@@ -18,6 +18,14 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
+## output the blocks in the d64 files, skipping the 3-sector hole at sector
+## $165 (created by (S#>S+T to skip over BAM and directory)
+function d64blocks() {
+    dd bs=256 count=$((0x165)) iflag=fullblock
+    dd bs=256 count=3 of=/dev/null iflag=fullblock
+    dd bs=256 count=$((683 - 0x165 - 3)) iflag=fullblock
+}
+
 ## Start with empty .d81 image (800 KiB) with a proper BAM etc.
 rm -f empty.d81
 cbmconvert -D8 empty.d81 -d empty.d64
@@ -32,13 +40,18 @@ dd if=/dev/zero of=vforth4.d81 bs=2 count=$((3*17)) \
 dd if=/dev/zero of=vforth4.d81 bs=2 count=$((3*17)) \
    seek=$((0x61910/2+20*3)) conv=notrunc
 
-## forth blocks 400..569 correspond to tracks 41..57.  Mark those as in use via the
-## second BAM sector
-dd if=/dev/zero of=vforth4.d81 bs=2 count=$((3*17)) \
+## For 1581 disks we implement a 4-sector hole at the start of track
+## 40 to skip over BAM, directory etc.  See (s#>s+t (for 1541 disks
+## VolksForth adds a 3-sector hole to skip the BAM).  This introduces a
+## 1-block offset for everything coming thereafter!
+
+## forth blocks 400..569 correspond to tracks 41..58 with a 4-sector
+## offset, due to the hole. Mark those as in use via the second BAM sector
+dd if=/dev/zero of=vforth4.d81 bs=2 count=$((3*18)) \
    seek=$((0x61a10/2)) conv=notrunc
 
-## same for  blocks 600..769  (tracks 61..77)
-dd if=/dev/zero of=vforth4.d81 bs=2 count=$((3*17)) \
+## same for  blocks 600..769  (tracks 61..78)
+dd if=/dev/zero of=vforth4.d81 bs=2 count=$((3*18)) \
    seek=$((0x61a10/2+20*3)) conv=notrunc
 
 ## Now copy our CBM program files into the sectors marked unused in the BAM
@@ -46,8 +59,9 @@ cbmconvert -D8 vforth4.d81 -n ../cbmfiles/v4thblk-c65
 
 ## Now add the 4 vforth4*.d64 disk files with a stride of 200 blocks
 for idx in 1 2 3 4; do
-    dd if=vforth4_${idx}.d64 bs=1024 count=170 of=vforth4.d81 \
-       seek=$(((idx-1)*200)) conv=notrunc
+    d64blocks < vforth4_${idx}.d64 |
+	dd bs=1024 count=170 of=vforth4.d81 iflag=fullblock \
+	   seek=$(((idx-1)*200 + (idx>2? 1 : 0))) conv=notrunc
 done
 
 ## Blank the binary .PRG block parts of vforth4_1.d64
